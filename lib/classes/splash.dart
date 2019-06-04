04d158de4c5c2a166e55a3d93f7bcedeaa0df7cf
RevanWang <zjqx1991@163.com>
2019-06-04 11:29:24 +0800

    项目结构

---
+++ lib/classes/splash.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _SplashState();
+  }
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      color: Colors.yellow,
+      child: Stack(
+        children: <Widget>[
+
+        ],
+      ),
+    );
+  }
+}
